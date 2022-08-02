8a3565d617a864770f9f34327a392678d61be335
aittuandung <tuandung.ait.it@gmail.com>
2022-08-02 23:14:19 +0700

    add

---
+++ src/main/java/controller/filter/CheckLogin.java
@@ -0,0 +1,39 @@
+package controller.filter;
+
+import dao.AccountDao;
+import model.Account;
+import model.Login;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.time.LocalDate;
+
+@WebFilter(urlPatterns = {"/student"})
+public class CheckLogin implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (Login.account == null) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            if (Login.account.getRole().equals("user")) {
+                chain.doFilter(request, response);
+            } else {
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/error.jsp");
+                dispatcher.forward(request, response);
+            }
+        }
+    }
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
+
